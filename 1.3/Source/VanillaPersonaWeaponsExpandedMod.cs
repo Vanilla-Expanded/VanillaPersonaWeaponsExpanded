b4f8468c45e4d20830bbb6ffbe1317d47d2201c3
Taranchuk <nfhfyxer@gmail.com>
2022-06-25 08:00:49 +0300

    1.3 folder

---
+++ 1.3/Source/VanillaPersonaWeaponsExpandedMod.cs
@@ -0,0 +1,31 @@
+﻿using HarmonyLib;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace VanillaPersonaWeaponsExpanded
+{
+    public static class VPWE_DefOf
+    {
+        public static RoyalTitleDef Baron;
+    }
+    public class VanillaPersonaWeaponsExpandedMod : Mod
+    {
+        public VanillaPersonaWeaponsExpandedMod(ModContentPack pack) : base(pack)
+        {
+            new Harmony("VanillaPersonaWeaponsExpanded.Mod").PatchAll();
+        }
+    }
+
+    [HarmonyPatch(typeof(Pawn_RoyaltyTracker), "TryUpdateTitle")]
+    public static class Pawn_RoyaltyTracker_TryUpdateTitle
+    {
+        public static void Postfix(bool __result, Faction faction, bool sendLetter = true, RoyalTitleDef updateTo = null)
+        {
+            if (__result && updateTo == VPWE_DefOf.Baron)
+            {
+                
+            }
+        }
+    }
+}
