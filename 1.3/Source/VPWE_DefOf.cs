0918fd0db603c3fb2d893cdeeabfa3e1e14ff6e0
Taranchuk <nfhfyxer@gmail.com>
2022-06-25 11:14:16 +0300

    mostly finished

---
+++ 1.3/Source/VPWE_DefOf.cs
@@ -0,0 +1,12 @@
+﻿using RimWorld;
+using Verse;
+
+namespace VanillaPersonaWeaponsExpanded
+{
+    [DefOf]
+    public static class VPWE_DefOf
+    {
+        public static RoyalTitleDef Baron;
+        public static LetterDef VPWE_ChoosePersonaWeapon;
+    }
+}
--- 1.3/Source/VanillaPersonaWeaponsExpandedMod.cs
+++ 1.3/Source/VanillaPersonaWeaponsExpandedMod.cs
@@ -5,10 +5,6 @@ using Verse;
 
 namespace VanillaPersonaWeaponsExpanded
 {
-    public static class VPWE_DefOf
-    {
-        public static RoyalTitleDef Baron;
-    }
     public class VanillaPersonaWeaponsExpandedMod : Mod
     {
         public VanillaPersonaWeaponsExpandedMod(ModContentPack pack) : base(pack)
@@ -16,15 +12,21 @@ namespace VanillaPersonaWeaponsExpanded
             new Harmony("VanillaPersonaWeaponsExpanded.Mod").PatchAll();
         }
     }
-
-    [HarmonyPatch(typeof(Pawn_RoyaltyTracker), "TryUpdateTitle")]
-    public static class Pawn_RoyaltyTracker_TryUpdateTitle
+    
+    [HarmonyPatch(typeof(Pawn_RoyaltyTracker), "OnPostTitleChanged")]
+    public static class Pawn_RoyaltyTracker_OnPostTitleChanged
     {
-        public static void Postfix(bool __result, Faction faction, bool sendLetter = true, RoyalTitleDef updateTo = null)
+        public static void Postfix(Pawn_RoyaltyTracker __instance, Faction faction, RoyalTitleDef prevTitle, RoyalTitleDef newTitle)
         {
-            if (__result && updateTo == VPWE_DefOf.Baron)
+            if ((prevTitle is null || prevTitle.seniority < newTitle.seniority) && (newTitle == VPWE_DefOf.Baron 
+                || prevTitle is null && newTitle.seniority > VPWE_DefOf.Baron.seniority) && faction == Faction.OfEmpire)
             {
-                
+                var letter = LetterMaker.MakeLetter("VPWE.GainedPersonaWeaponTitle".Translate(__instance.pawn.Named("PAWN")),
+                    "VPWE.GainedPersonaWeaponDesc".Translate(__instance.pawn.Named("PAWN"), newTitle.GetLabelFor(__instance.pawn.gender)),
+                    VPWE_DefOf.VPWE_ChoosePersonaWeapon, faction) as ChoiceLetter_ChoosePersonaWeapon;
+                letter.pawn = __instance.pawn;
+                Find.LetterStack.ReceiveLetter(letter);
+                Current.Game.GetComponent<GameComponent_PersonaWeapons>().unresolvedLetters.Add(letter);
             }
         }
     }
